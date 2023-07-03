403cfc65b9a45a1cb2e3c1f6cb68a118845bdc66
LENOVO <amar13617@gmail.com>
2023-07-03 21:20:33 +0530

    mutable python committed

---
+++ Mutable.py
@@ -0,0 +1,67 @@
+
+friends_last_seen = {
+    'Rolf': 31,
+    'Jen': 1,
+    'Anne': 7
+}
+
+
+
+print(id(friends_last_seen))
+
+friends_last_seen = {
+    'Rolf': 31,
+    'Jen': 1,
+    'Anne': 7
+}
+
+print(id(friends_last_seen))
+
+
+friends_last_seen = {
+    'Rolf': 31,
+    'Jen': 1,
+    'Anne': 7
+}
+
+print(id(friends_last_seen))
+
+friends_last_seen['Anne'] = 20
+
+print(id(friends_last_seen))
+
+
+
+age = 20
+print(id(age))
+
+
+age = 30
+print(id(age))
+
+
+
+greeting = 'hello'
+print(id(greeting))
+
+greeting = 'byebye'
+print(id(greeting))
+
+greeting = greeting + 'a'
+print(id(greeting))
+
+
+my_tuple = (3, 5, 10)
+print(id(my_tuple))
+
+my_tuple = (3, 5)
+print(id(my_tuple))
+
+
+
+friends = ['Rolf', 'Jose']
+print(id(friends))
+
+friends.append('Jen')
+print(id(friends))
+
